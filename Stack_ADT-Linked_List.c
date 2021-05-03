fdcda4ea03d2bbc9202d3b2b55755d4a0cc59ba2
praabindhp <praabindhp@gmail.com>
2021-05-03 18:57:48 +0530

    Updates for 2021-05-03

---
+++ Stack_ADT-Linked_List.c
@@ -0,0 +1,88 @@
+#include<stdio.h>
+int stack[100],choice,n,top,x,i;
+void push(void);
+void pop(void);
+void display(void);
+int main()
+{
+    //clrscr();
+    top=-1;
+    printf("\n Enter Size Of STACK[MAX=100]:");
+    scanf("%d",&n);
+    printf("\n\t STACK OPERATIONS USING ARRAY");
+    printf("\n\t--------------------------------");
+    printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.DISPLAY\n\t 4.EXIT");
+    do
+    {
+        printf("\n Enter Choice:");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            {
+                push();
+                break;
+            }
+            case 2:
+            {
+                pop();
+                break;
+            }
+            case 3:
+            {
+                display();
+                break;
+            }
+            case 4:
+            {
+                printf("\n\tSUCCESSFUL EXIT");
+                break;
+            }
+            default:
+            {
+                printf ("\n\t Please Enter Valid Choice(1/2/3/4)");
+            }                 
+        }
+    }
+    while(choice!=4);
+    return 0;
+}
+void push()
+{
+    if(top>=n-1)
+    {
+        printf("\n\tSTACK Over Flow");
+    }
+    else
+    {
+        printf(" Enter Value To Be Pushed:");
+        scanf("%d",&x);
+        top++;
+        stack[top]=x;
+    }
+}
+void pop()
+{
+    if(top<=-1)
+    {
+        printf("\n\t Stack Under Flow");
+    }
+    else
+    {
+        printf("\n\t The Popped Elements : %d",stack[top]);
+        top--;
+    }
+}
+void display()
+{
+    if(top>=0)
+    {   printf("\n The Elements In STACK \n");
+        for(i=top; i>=0; i--)
+            printf("\n%d",stack[i]);
+        printf("\n Press Next Choice");
+    }
+    else
+    {
+        printf("\n The STACK Is Empty");
+    }    
+}
